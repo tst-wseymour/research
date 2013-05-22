e18f0037256acbcb1e4f8e9e87e46ee06fc23512
wayneseymour <wseymour@t-sciences.com>
2013-05-22 04:46:32 -0400

    - got the accordion running correctly

---
+++ gwt/GwtMvnSpike/src/main/java/research/gwtmvnspike/client/AccordionContainer.java
@@ -0,0 +1,78 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package research.gwtmvnspike.client;
+
+import com.extjs.gxt.ui.client.widget.ContentPanel;
+import com.extjs.gxt.ui.client.widget.HtmlContainer;
+import com.extjs.gxt.ui.client.widget.layout.AccordionLayout;
+
+/**
+ *
+ * @author WSeymour
+ */
+public class AccordionContainer {
+
+    public HtmlContainer makeLinks(String[] links) {
+        StringBuilder sb = new StringBuilder("<ul class='accordion-list'>");
+        for (String link : links) {
+            sb.append("<li>").append(link).append("</li>");
+        }
+        sb.append("</ul>");
+        HtmlContainer html = new HtmlContainer(sb.toString());
+        return html;
+    }
+
+    public ContentPanel getContainer() {
+        // create the accordion
+        ContentPanel accordionCt = new ContentPanel();
+        accordionCt.setSize(180, 200);
+        accordionCt.setHeadingHtml("Library Widget");
+        accordionCt.setBodyBorder(false);
+        accordionCt.setLayout(new AccordionLayout());
+
+        // add the products panel
+        ContentPanel panel = new ContentPanel();
+        panel.setHeadingHtml("SOPs");
+        accordionCt.add(panel);
+
+        // put links into "products"
+        String[] links = new String[]{"view", "create", "search"};
+        panel.add(makeLinks(links));
+
+        // add the sales panel
+        panel = new ContentPanel();
+        panel.setHeadingHtml("User Guides");
+        accordionCt.add(panel);
+
+        // put links into "sales"
+        links = new String[]{"orders", "returns", "invoices"};
+        panel.add(makeLinks(links));
+
+        // add the reports panel
+        panel = new ContentPanel();
+        panel.setHeadingHtml("How Tos");
+        accordionCt.add(panel);
+
+        // put links into "reports"
+        links = new String[]{"summary", "stock", "Ad-hoc"};
+        panel.add(makeLinks(links));
+
+//        // add the issues panel
+//        panel = new ContentPanel();
+//        // setAnimCollapse(false);
+//        panel.setHeadingHtml("Issues");
+//        panel.setBodyStyle("padding:10px;");
+//        panel.addText("<p>we don't have any <i>issues</i> right ...<p>");
+//        accordionCt.add(panel);
+        /*
+         * GxtCookbk is the application's entry point class. We access its main
+         * content panel using the static GxtCookBk.getAppCenterPanel() call. We
+         * add our panel to the main content panel.
+         */
+//        GxtCookBk.getAppCenterPanel().add(accordionCt);
+        return accordionCt;
+    }
+
+}
--- gwt/GwtMvnSpike/src/main/java/research/gwtmvnspike/client/Module1.java
+++ gwt/GwtMvnSpike/src/main/java/research/gwtmvnspike/client/Module1.java
@@ -10,6 +10,7 @@ import com.extjs.gxt.ui.client.widget.VerticalPanel;
 import com.extjs.gxt.ui.client.widget.Window;
 import com.extjs.gxt.ui.client.widget.button.Button;
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.RootPanel;
 
 /**
  * Entry point classes define
@@ -17,70 +18,78 @@ import com.google.gwt.core.client.EntryPoint;
  */
 public class Module1 implements EntryPoint {
 
+    private static LayoutContainer appCenter = new LayoutContainer();
 
+    public static LayoutContainer getAppCenterPanel() {
+        return appCenter;
+    }
 
     /**
      * This is the entry point method.
      */
     public void onModuleLoad() {
-        // dialog window
-        Dialog dialog = new Dialog();
-        dialog.setBodyBorder(false);
-        dialog.setClosable(false);
-        dialog.setHideOnButtonClick(true);
-        dialog.setButtons(Dialog.OKCANCEL);
-        dialog.setScrollMode(Scroll.NONE);
-        dialog.setHeadingHtml("GXT Cookbook :: Recipe Three");
-        dialog.addText("Dialogs are descendants of the Window class, so they are windows that can do even more.");
-        dialog.show();
-        SelectionListener<ButtonEvent> listener = new SelectionListener<ButtonEvent>() {
+//        // dialog window
+//        Dialog dialog = new Dialog();
+//        dialog.setBodyBorder(false);
+//        dialog.setClosable(false);
+//        dialog.setHideOnButtonClick(true);
+//        dialog.setButtons(Dialog.OKCANCEL);
+//        dialog.setScrollMode(Scroll.NONE);
+//        dialog.setHeadingHtml("GXT Cookbook :: Recipe Three");
+//        dialog.addText("Dialogs are descendants of the Window class, so they are windows that can do even more.");
+//        dialog.show();
+//        SelectionListener<ButtonEvent> listener = new SelectionListener<ButtonEvent>() {
+//
+//            @Override
+//            public void componentSelected(ButtonEvent evt) {
+//                String text = evt.getButton().getHtml();
+//                String format = "You clicked the {0} button";
+//                Info.display("Recipe Three", format, text);
+//            }
+//        };
+//
+//        Button okBtn = dialog.getButtonById(Dialog.OK);
+//        okBtn.addSelectionListener(listener);
+//        Button cancelBtn = dialog.getButtonById(Dialog.OK);
+//        cancelBtn.addSelectionListener(listener);
+//
+//
+//
+//        // basic window setup
+//        Window xWindow = new Window();
+//        xWindow.setHeadingHtml("GXT CookBook | Recipe One");
+//        xWindow.setClosable(true);
+//        xWindow.setSize(350, 170);
+//        xWindow.setModal(true);
+//        xWindow.setBlinkModal(true);
+//        xWindow.setResizable(true);
+//        xWindow.setMaximizable(true);
+//
+//
+//        // prepare content to show
+//        LayoutContainer textPanel = new VerticalPanel();
+//        textPanel.setStyleAttribute("padding", "15px");
+//        textPanel.addText("This is our first recipe from GXT Cookbok, how are we doing so far ...");
+//
+//        // some content
+//        StringBuilder msg = new StringBuilder();
+//        msg.append("This window can do lots of stuff.");
+//
+//        // constrain maximize surface to container, not browser's complete viewable area
+//        xWindow.setContainer(textPanel.getElement());
+//        // constrain draggable to the same as above
+//        xWindow.getDraggable().setContainer(textPanel);
 
-            @Override
-            public void componentSelected(ButtonEvent evt) {
-                String text = evt.getButton().getHtml();
-                String format = "You clicked the {0} button";
-                Info.display("Recipe Three", format, text);
-            }
-           
-        };
-        
-        Button okBtn = dialog.getButtonById(Dialog.OK);
-        okBtn.addSelectionListener(listener);
-        Button cancelBtn = dialog.getButtonById(Dialog.OK);
-        cancelBtn.addSelectionListener(listener);
-        
-        
-        
-        // basic window setup
-        Window xWindow = new Window();
-        xWindow.setHeadingHtml("GXT CookBook | Recipe One");
-        xWindow.setClosable(true);
-        xWindow.setSize(350, 170);
-        xWindow.setModal(true);
-        xWindow.setBlinkModal(true);
-        xWindow.setResizable(true);
-        xWindow.setMaximizable(true);
-        
-        
-        // prepare content to show
-        LayoutContainer textPanel = new VerticalPanel();
-        textPanel.setStyleAttribute("padding", "15px");
-        textPanel.addText("This is our first recipe from GXT Cookbok, how are we doing so far ...");
-        
-        // some content
-        StringBuilder msg = new StringBuilder();
-        msg.append("This window can do lots of stuff.");
-        
-        // constrain maximize surface to container, not browser's complete viewable area
-        xWindow.setContainer(textPanel.getElement());
-        // constrain draggable to the same as above
-        xWindow.getDraggable().setContainer(textPanel);        
-        
-        
+
+        // 
+        AccordionContainer accordionContainer = new AccordionContainer();
+        RootPanel.get().add(accordionContainer.getContainer());
+
+
 
         // place content on the window
         // and display it.
-        xWindow.add(textPanel);
-        xWindow.show();
+//        xWindow.add(textPanel);
+//        xWindow.show();
     }
 }

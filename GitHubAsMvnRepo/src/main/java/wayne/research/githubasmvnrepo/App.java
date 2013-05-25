3ecf84bdb6c7c5f5d26c593cb6a9577f86c6d87c
Wayne Seymour <wseymour@t-sciences.com>
2013-05-25 06:42:11 -0400

    Adding the spike project I used to build the github based maven repo.

---
+++ GitHubAsMvnRepo/src/main/java/wayne/research/githubasmvnrepo/App.java
@@ -0,0 +1,13 @@
+package wayne.research.githubasmvnrepo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
